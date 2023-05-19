53479af66ff479ee72a01fbe9ac6fe960c9fd2ce
arcegor <yvegorkv@yandex.ru>
2023-05-19 23:24:30 +0300

    чтобы не потерять

--- FileManager.py
+++ FileManager.py
@@ -1,31 +1,37 @@
 import gc
 import os
 
+import pandas as pd
+
 from ProcessManager import ProcessManager
+import openpyxl
 
 
 class FileManager(object):
 
     def __init__(self):
         self.data = {}
+        self.styles = {}
         self.result = {}
         self.pm = ProcessManager()
         self.source_filenames = {}
+        self.flag_source = False
+        self.flag_target = False
 
     def validate(self) -> bool:
         if self.data:
             return True
 
-    def get_result(self) -> list:
-        keys = self.result.keys()
+    def get_result(self, flag: str):
+        keys = list(self.result.keys())
         path = os.getcwd() + '/RESULT'
         if not os.path.exists(path):
             os.makedirs(path)
-        for key in keys:
-            name = self.pm.generate_name(self.source_filenames, key)
+        if flag in keys:
+            name = self.pm.generate_name(self.source_filenames, flag)
             filepath = os.path.join(path, name)
-            self.result[key].to_excel(filepath)
-        return list(keys)
+            self.result[flag].to_excel(filepath)
+        return True
 
     def preprocess(self):
         keys = list(self.data.keys())
@@ -35,8 +41,8 @@ class FileManager(object):
         self.data['target'] = self.data.pop(keys[0])
 
     def process(self):
-        self.preprocess()
-        self.result = self.pm.find_collisions(self.data)
+        # self.preprocess()
+        # self.result = self.pm.find_collisions(self.data)
         return True
 
     def clear(self):
--- main.py
+++ main.py
@@ -8,7 +8,6 @@ from PyQt5.QtWidgets import QFileDialog, QMessageBox
 from ui import Ui_Upload
 from FileManager import FileManager
 import pandas as pd
-import openpyxl
 
 
 class MainWindow(QtWidgets.QMainWindow):
@@ -17,38 +16,50 @@ class MainWindow(QtWidgets.QMainWindow):
         super().__init__()
         self.ui = Ui_Upload()
         self.ui.setupUi(self)
-        self.ui.buttonUpload.clicked.connect(self.buttonUpload_clicked)
-        self.ui.buttonProcess.clicked.connect(self.buttonProcess_clicked)
-        self.ui.buttonGet.clicked.connect(self.buttonGet_clicked)
-        self.ui.buttonProcess.setDisabled(True)
-        self.ui.buttonGet.setDisabled(True)
+
+        # Здесь можно настроить html разметку для приветствия
+        self.html = '<h3>Добро пожаловать!</h3>'
+
+        # Кнопки
+        self.ui.buttonChooseSource.clicked.connect(self.buttonChooseSource_clicked)
+        self.ui.buttonFindCollisions.clicked.connect(self.buttonFindCollisions_clicked)
+        self.ui.buttonDownloadFixed.clicked.connect(self.buttonDownloadFixed_clicked)
+        self.ui.buttonDownloadReport.clicked.connect(self.buttonDownloadReport_clicked)
+        self.ui.buttonChooseTarget.clicked.connect(self.buttonChooseTarget_clicked)
+        self.ui.buttonReset.clicked.connect(self.buttonReset_clicked)
+        self.ui.buttonFindCollisions.setDisabled(True)
+        self.ui.buttonDownloadFixed.setDisabled(True)
+        self.ui.buttonDownloadReport.setDisabled(True)
+        self.ui.buttonChooseTarget.setDisabled(True)
         self.fm = FileManager()
 
-    def buttonUpload_clicked(self):
-        filenames, _ = QFileDialog.getOpenFileNames(
-            self,
-            'Open file',
-            './~',
-            "(*.xls *.xlsx *.xlsm)"
-        )
-        if filenames:
-            filenames = [str(Path(filename)) for filename in filenames]
-        else:
-            return
-        for filename in filenames:
-            self.fm.data[filename] = pd.read_excel(filename)
-        if not self.fm.validate():
-            msg = QMessageBox()
-            msg.setIcon(QMessageBox.Critical)
-            msg.setText("Документы не соответствуют шаблону!")
-            msg.setWindowTitle("Ошибка валидации!")
-            msg.exec_()
-            return
-        self.ui.files.setText('\n'.join(str(x) for x in self.fm.data.keys()))
-        self.ui.buttonProcess.setEnabled(True)
-        self.ui.buttonUpload.setDisabled(True)
+        self.ui.SomeInfo.setHtml(self.html)
 
-    def buttonProcess_clicked(self):
+    def buttonChooseSource_clicked(self):
+        filename, _ = QFileDialog.getOpenFileName(self, 'Open file', './~', '(*.xls *.xlsx *.xlsm)')
+        if not filename:
+            return
+        tmp = pd.ExcelFile.OpenpyxlReader(filename)
+        # self.fm.data['source'] = tmp.parse()
+        df = tmp.parse()
+        style = tmp.book
+        df.to_excel(filename, storage_options=style)
+        if self.is_valid():
+            self.ui.buttonChooseTarget.setEnabled(True)
+            self.ui.buttonChooseSource.setDisabled(True)
+            self.ui.SomeInfo.append(f'Эталонный файл {filename} успешно загружен!\nВыберите проверяемый файл\n')
+
+    def buttonChooseTarget_clicked(self):
+        filename, _ = QFileDialog.getOpenFileName(self, 'Open file', './~', '(*.xls *.xlsx *.xlsm)')
+        if not filename:
+            return
+        self.fm.data['target'] = pd.read_excel(filename)
+        if self.is_valid():
+            self.ui.buttonChooseTarget.setDisabled(True)
+            self.ui.SomeInfo.append(f'Проверяемый файл {filename} успешно загружен!\nТеперь можно провести проверку!\n')
+            self.ui.buttonFindCollisions.setEnabled(True)
+
+    def buttonFindCollisions_clicked(self):
         if not self.fm.process():
             msg = QMessageBox()
             msg.setIcon(QMessageBox.Critical)
@@ -56,15 +67,38 @@ class MainWindow(QtWidgets.QMainWindow):
             msg.setWindowTitle("Ошибка!")
             msg.exec_()
             return
-        self.ui.buttonGet.setEnabled(True)
-        self.ui.buttonProcess.setDisabled(True)
+        self.ui.buttonDownloadReport.setEnabled(True)
+        self.ui.buttonDownloadFixed.setEnabled(True)
+        self.ui.buttonFindCollisions.setDisabled(True)
 
-    def buttonGet_clicked(self):
-        self.fm.get_result()
+    def buttonDownloadFixed_clicked(self):
+        if self.fm.get_result('target'):
+            self.ui.SomeInfo.setText('\n'.join(str(x) for x in self.fm.result.keys()))
+            self.ui.buttonDownloadFixed.setDisabled(True)
+
+    def buttonDownloadReport_clicked(self):
+        if self.fm.get_result('source'):
+            self.ui.SomeInfo.setText('\n'.join(str(x) for x in self.fm.result.keys()))
+            self.ui.buttonDownloadFixed.setDisabled(True)
+
+    def is_valid(self):
+        if not self.fm.validate():
+            msg = QMessageBox()
+            msg.setIcon(QMessageBox.Critical)
+            msg.setText("Документы не соответствуют шаблону!")
+            msg.setWindowTitle("Ошибка валидации!")
+            msg.exec_()
+            return False
+        return True
+
+    def buttonReset_clicked(self):
         self.fm.clear()
-        self.ui.files.setText('\n'.join(str(x) for x in self.fm.result.keys()))
-        self.ui.buttonUpload.setEnabled(True)
-        self.ui.buttonGet.setDisabled(True)
+        self.ui.buttonDownloadFixed.setDisabled(True)
+        self.ui.buttonDownloadFixed.setDisabled(True)
+        self.ui.buttonFindCollisions.setDisabled(True)
+        self.ui.buttonChooseSource.setEnabled(True)
+        self.ui.buttonChooseTarget.setDisabled(True)
+        self.ui.SomeInfo.setHtml(self.html)
 
 
 if __name__ == '__main__':
